8d586266757f981d13be0f7322dc72ac1a61daea
Azharnaeem <azharnaeem76@gmail.com>
2020-05-02 10:07:33 +0500

    cart added

--- Closures/clos.js
+++ Closures/clos.js
@@ -1,1 +1,15 @@
-var a = document.createElement('div');
+
+
+
+function cart(){
+    var a = document.getElementById("crt");
+    a.style.opacity=1;
+    a.style.top='110px';
+}
+
+function clos(){
+    var b = document.getElementById("crt");
+    b.style.opacity=0;
+    b.style.marginTop='250px';
+}
+
